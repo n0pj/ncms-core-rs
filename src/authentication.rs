9aadc0de211132dd84a3ae3246aa707034a6c194
n0pj <admin@n0pj.com>
2022-01-21 17:38:38 +0900

    パスワード関係追加

---
+++ src/authentication.rs
@@ -0,0 +1,36 @@
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Argon2,
+};
+
+pub struct Password;
+
+///
+/// パスワードのハッシュ化と確認用
+///
+impl Password {
+    ///
+    /// https://github.com/RustCrypto/password-hashes/tree/master/argon2
+    ///
+    pub fn gen_password_hash(raw_password: &str) -> Result<String, argon2::password_hash::Error> {
+        let salt = SaltString::generate(&mut OsRng);
+        let argon2 = Argon2::default();
+        let password_hash = argon2
+            .hash_password(raw_password.as_bytes(), &salt)?
+            .to_string();
+        println!("{}", password_hash);
+
+        Ok(password_hash)
+    }
+
+    pub fn verify_password(
+        input_password: &str,
+        saved_password: &str,
+    ) -> Result<bool, argon2::password_hash::Error> {
+        let parsed_hash = PasswordHash::new(saved_password)?;
+
+        Ok(Argon2::default()
+            .verify_password(input_password.as_bytes(), &parsed_hash)
+            .is_ok())
+    }
+}
