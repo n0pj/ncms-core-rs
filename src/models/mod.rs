17ddb846a8555a2ca774007b930bfa548cb8770d
n0pj <admin@n0pj.com>
2022-02-13 01:35:11 +0900

    trait の構造を変更

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -41,7 +41,7 @@ impl fmt::Display for Order {
 }
 
 ///
-/// DB のテーブルの作成モデル ( Fetch ) に対して実装するメソッド
+/// DB のテーブルの取得モデル ( Fetch ) に対して実装するメソッド
 ///
 pub trait Model<M, R, E> {
     fn to_res(&self) -> Result<R, E>;
@@ -51,9 +51,11 @@ pub trait Model<M, R, E> {
 /// DB のテーブルの作成モデル ( New ) に対して実装するメソッド
 ///
 pub trait NewModel<M, N, E> {
+    fn from_model(model: &M) -> Result<N, E>;
     fn to_model(&self) -> Result<M, E>;
     fn insert(&self) -> Result<M, E>;
 
+    ///
     /// update する際は、API の引数ですべてを受け取り、その受け取った引数から NewModel を作成して update
     fn update(&self) -> Result<M, E>;
 }
