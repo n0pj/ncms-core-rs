b59a4d1626e34bc80076b2ce1c78f7c621b8e0fc
n0pj <admin@n0pj.com>
2022-02-15 19:35:32 +0900

    const HEADERS 追加

--- src/errors/validation.rs
+++ src/errors/validation.rs
@@ -9,3 +9,8 @@ pub const CANNOT_FIND_QUERY: Error = Error {
     code: "CANNOT_FIND_QUERY",
     message: "Can not find query parameter.",
 };
+
+pub const CANNOT_FIND_HEADERS: Error = Error {
+    code: "CANNOT_FIND_HEADERS",
+    message: "Can not find headers.",
+};
