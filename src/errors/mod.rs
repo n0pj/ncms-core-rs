600e209d7945cac5311bac9f8fe7adf2be700e67
n0pj <admin@n0pj.com>
2022-02-22 10:54:54 +0900

    Error impl

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -4,10 +4,33 @@ pub mod http;
 pub mod validation;
 
 use serde::{Deserialize, Serialize};
+use std::fmt::Display;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Error<'a> {
-    pub property: &'a str,
-    pub code: &'a str,
-    pub message: &'a str,
+pub struct Error {
+    pub property: String,
+    pub code: String,
+    pub message: String,
+}
+
+impl Error {
+    pub fn new<T: Display>(property: T, code: T, message: T) -> Self {
+        Self {
+            property: property.to_string(),
+            code: code.to_string(),
+            message: message.to_string(),
+        }
+    }
+
+    pub fn to_value(&self) -> serde_json::Value {
+        serde_json::json!({
+            "property": self.property,
+            "code": self.code,
+            "message": self.message,
+        })
+    }
+
+    pub fn to_string(&self) -> String {
+        serde_json::to_string(&self.to_value()).unwrap()
+    }
 }
--- src/errors/validation.rs
+++ src/errors/validation.rs
@@ -1,19 +1,7 @@
 use crate::errors::Error;
 
-pub const CANNOT_FIND_QUERY_STRING_PARAMETERS: Error = Error {
-    property: "queryStringParameters",
-    code: "CANNOT_FIND_QUERY_STRING_PARAMETERS",
-    message: "Can not find queryStringParameters parameter.",
-};
-
-pub const CANNOT_FIND_QUERY: Error = Error {
-    property: "query",
-    code: "CANNOT_FIND_QUERY",
-    message: "Can not find query parameter.",
-};
-
-pub const CANNOT_FIND_HEADERS: Error = Error {
-    property: "headers",
-    code: "CANNOT_FIND_HEADERS",
-    message: "Can not find headers.",
-};
+pub enum ValidationError {
+    CannotFindQueryStringParameters,
+    CannotFindQuery,
+    CannotFindHeaders,
+}
